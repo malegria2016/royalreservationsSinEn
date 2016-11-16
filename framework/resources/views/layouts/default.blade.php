d4fbbbb4d13c4507a84b854073c399e9bb3dab88
Gabriel Garcia <gabriel@gconde.com>
2016-11-16 16:46:03 -0500

    cambio de posicion tag manager

--- framework/resources/views/layouts/default.blade.php
+++ framework/resources/views/layouts/default.blade.php
@@ -37,6 +37,16 @@
 	</head>
 
 	<body>
+	
+		<!-- Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NLLFCQ"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-NLLFCQ');</script>
+		<!-- End Google Tag Manager -->
 
 
 		<!--<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
@@ -81,15 +91,7 @@
 		<script type="text/javascript" src="{{ asset('js/main.min.js') }}"></script>
 
 		@yield('javascript')
-		<!-- Google Tag Manager -->
-		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NLLFCQ"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-NLLFCQ');</script>
-		<!-- End Google Tag Manager -->
+		
 
 		<!-- MOUSE FLOW -->
 		<script type="text/javascript">
